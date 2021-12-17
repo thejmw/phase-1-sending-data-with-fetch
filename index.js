996a787e0834fdd56ec81e517a71835384c33c48
Jason <thejasonmichaelwilliams@gmail.com>
2021-12-17 17:39:54 -0500

    commit tree 7ee7a90138639b93c1f937916955bb5338024421

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+const body = document.getElementsByTagName('body')[0];
+
+function submitData(firstName, email){
+    const configObject = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+    body: JSON.stringify({
+        'name': firstName,
+        'email': email
+        })
+    };
+return fetch('http://localhost:3000/users', configObject)
+.then (response => response.json())
+.then (body => addNewContact(body.id))
+.catch (function (error){
+    let message = 'Unauthorized Access';
+    alert('Unauthorized Access');
+    document.body.innerHTML = error.message
+});
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName('body')[0].innerHTML = newID;
+}
+
+function errorFunc(error) {
+    document.body.innerHTML = error.message
+}
+
+submitData('Name', 'name@name.com')
+
